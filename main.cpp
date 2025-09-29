9cac0f86ccaea9dd8c15da9bfd8c34b92772d27f
GingerNinjaProgramming <connervian@outlook.com>
2025-09-29 14:23:07 +0100

    Circle Moves
    The circle Moves

--- main.cpp
+++ main.cpp
@@ -3,11 +3,30 @@
 
 using namespace std;
 
+
+
 int main () {
+    int LocX = 0;
+    int LocY = 0;
+
     InitWindow(800,800,"Random super cool circle DLC expansion");
-    SetTargetFPS(120);
+    SetTargetFPS(60);
 
-    if(!WindowShouldClose()){
+    while(!WindowShouldClose()){
+        if(IsKeyDown(KEY_A)) LocX += -10;
+        else if(IsKeyDown(KEY_S)) LocY += 10;
+        else if(IsKeyDown(KEY_W)) LocY += -10;
+        else if(IsKeyDown(KEY_D)) LocX += 10;
 
+
+        BeginDrawing();
+        ClearBackground(BLUE);
+        DrawCircle(LocX,LocY,20,WHITE);
+        EndDrawing();
     }
+
+    CloseWindow();
 }
+
+
+
