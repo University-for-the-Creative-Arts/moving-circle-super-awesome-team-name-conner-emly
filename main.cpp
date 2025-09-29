516fc6963515aab0b4d252cdbe9316688b1f4a8b
GingerNinjaProgramming <connervian@outlook.com>
2025-09-29 14:29:35 +0100

    Added GameObject
    Added a game object class to represent objects in the game
    
    Right now it essentially a vector 2 contained by ref

--- main.cpp
+++ main.cpp
@@ -3,25 +3,36 @@
 
 using namespace std;
 
+class GameObject{
+    public:
+        int x;
+        int y;
 
+        GameObject(int _x, int _y){
+            this -> x = _x;
+            this -> y = _y;
+        }
+};
 
 int main () {
-    int LocX = 0;
-    int LocY = 0;
+
+    GameObject player(0,0);
 
     InitWindow(800,800,"Random super cool circle DLC expansion");
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
-        if(IsKeyDown(KEY_A)) LocX += -10;
-        else if(IsKeyDown(KEY_S)) LocY += 10;
-        else if(IsKeyDown(KEY_W)) LocY += -10;
-        else if(IsKeyDown(KEY_D)) LocX += 10;
+
+        if(IsKeyDown(KEY_A)) player.x += -10;
+        else if(IsKeyDown(KEY_S)) player.y += 10;
+        else if(IsKeyDown(KEY_W)) player.y += -10;
+        else if(IsKeyDown(KEY_D)) player.x += 10;
+
 
 
         BeginDrawing();
         ClearBackground(BLUE);
-        DrawCircle(LocX,LocY,20,WHITE);
+        DrawCircle(player.x,player.y,20,WHITE);
         EndDrawing();
     }
 
