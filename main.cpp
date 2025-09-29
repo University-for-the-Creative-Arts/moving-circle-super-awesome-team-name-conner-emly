9d751607e71ab35557cae4d5f063b8e017257281
GingerNinjaProgramming <connervian@outlook.com>
2025-09-29 12:46:57 +0100

    Airdrop Of Raylib
    Dropped in stuff to use raylib

--- main.cpp
+++ main.cpp
@@ -1,18 +1,43 @@
-#include "raylib.h"
+#include <iostream>
+#include <raylib.h>
 
-int main() {
-    const int screenWidth = 960;
-    const int screenHeight = 600;
+using namespace std;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Starter Template");
+int main () {
+
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+    int ball_x = 100;
+    int ball_y = 100;
+    int ball_speed_x = 5;
+    int ball_speed_y = 5;
+    int ball_radius = 15;
+
+    cout << "Hello World" << endl;
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
+    while (WindowShouldClose() == false){
+   
+        ball_x += ball_speed_x;
+        ball_y += ball_speed_y;
+
+        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
+        {
+            ball_speed_x *= -1;
+        }
+
+        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
+        {
+            ball_speed_y *= -1;
+        }
+        
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
+            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
         EndDrawing();
     }
 
     CloseWindow();
-    return 0;
 }
